fe5b5c17b86eae04292b9eabb04692b6c2c26d49
Aarchie05 <68143839+Aarchie05@users.noreply.github.com>
2021-02-20 01:07:31 +0530

    .

---
+++ Bouncing Ball/main.js
@@ -0,0 +1,19 @@
+$(document).ready(() => {
+    var ball = $(".ball");
+    ball.animate({marginLeft:"100px"},'slow');
+    ball.animate({marginTop:"0px"},'slow');
+
+    ball.animate({marginLeft:"200px"},'slow');
+    ball.animate({marginTop:"100px"},'slow');
+
+    ball.animate({marginLeft:"300px"},'slow');
+    ball.animate({marginTop:"200px"},'slow');
+    
+    ball.animate({marginLeft:"400px"},'slow');
+    ball.animate({marginTop:"300px"},'slow');
+
+    ball.animate({marginLeft:"500px"},'slow');
+    ball.animate({marginTop:"400px"},'slow');
+
+    ball.animate({marginLeft:"1400px"},'slow');
+});
